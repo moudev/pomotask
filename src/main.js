1a0e458ac064692712f19b04ddd0289921b1915f
moudev <rigobertomauriciomartinez@gmail.com>
2021-12-02 16:50:34 -0600

    feat: add basic timer control
    without style

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import 'windi.css'
 
+// https://day.js.org/docs/en/plugin/advanced-format
+import dayjs from "dayjs"
+import advancedFormat from 'dayjs/plugin/advancedFormat' // to get format in milliseconds
+dayjs.extend(advancedFormat)
+
+// https://vuejs.org/v2/cookbook/adding-instance-properties.html#The-Context-of-Prototype-Methods
+// https://medium.com/spemer/using-axios-in-vue-js-17f186756a8b
+Vue.prototype.$dayjs = dayjs
+
 Vue.config.productionTip = false
 
 new Vue({
