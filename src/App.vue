9d267ee71ef5d0830c5aa0a395791ac9e88586ff
moudev <rigobertomauriciomartinez@gmail.com>
2021-12-05 19:04:26 -0600

    refactor: update components structure

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,22 @@
 <template>
-  <div class="flex justify-around bg-main text-color-main overflow-hidden">
-    <Actions
-      class="border"
-      :isPlaying="isPlaying"
-      @startTimer="startTimer"
-      @stopTimer="stopTimer"
-    />
-    
-    <input
-      v-if="!isPlaying"
-      class="border bg-main"
-      type="text"
-      v-model="minutesToAdd"
-      :min="MIN_MINUTES"
-    />
-
-    <Timer
-      v-show="isPlaying"
-      class="border"
-      :minutesToAdd="minutesToAdd"
-      :isPlaying="isPlaying"
-      @stopTimer="stopTimer"
-    />
+  <div class="bg-main text-color-main overflow-hidden">
+    <Timer />
     <ToggleThemeButton />
-    <h1 class="text-color-main">Primary</h1>
-    <h2 class="text-color-secondary">Secondary</h2>
+    <TodoList />
   </div>
 </template>
 
 <script>
-import Actions from "./components/Actions.vue"
-import Timer from "./components/Timer.vue"
+import Timer from './components/Timer/Timer.vue'
+import TodoList from './components/TodoList/TodoList.vue'
 import ToggleThemeButton from './components/ToggleThemeButton.vue'
 
 export default {
   name: 'App',
   components: {
-    Actions,  
     Timer,
+    TodoList,
     ToggleThemeButton,
   },
-  data() {
-    return {
-      isPlaying: false,
-      minutesToAdd: "0",
-      MIN_MINUTES: 1,
-      MAX_MINUTES: 60,
-    }
-  },
-  methods: {
-    startTimer() {
-      if (this.minutesToAdd >= this.MIN_MINUTES && this.minutesToAdd <= this.MAX_MINUTES) {
-        this.isPlaying = true
-      }
-    },
-    stopTimer() {
-      this.isPlaying = false
-    }
-  }
 }
 </script>
---
+++ src/components/TodoList/TodoList.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>TodoList.vue</h1>
+</template>
