af9e307a1529f2c411bdda28cfc6eb791338f609
moudev <rigobertomauriciomartinez@gmail.com>
2021-12-09 18:05:21 -0600

    feat(todo-list): add drag and drop

--- src/components/TodoList/TodoList.vue
+++ src/components/TodoList/TodoList.vue
@@ -4,12 +4,15 @@
       <input type="text" placeholder="task" v-model="taskInput" />  
       <button class="border p-2" @click="addTask">Add</button>  
     </div>  
-    <div v-if="tasks.length > 0">
+    <div v-if="tasks.length > 0" @drop="drop($event)" @dragover.prevent @dragenter.prevent>
       <div
-        v-for="task in tasks"
+        v-for="(task, taskIndex) in tasks"
         :key="task.id"
         :class="{ 'line-through text-color-secondary': task.completed }"
         class="border flex justify-around items-center cursor-pointer hover:bg-secondary"
+        draggable
+        @dragstart="dragstart($event, taskIndex)"
+        @dragenter="dragenter($event, taskIndex)"
       >
         <div class="flex justify-items-center">
           <CheckboxCheckedFilled v-if="task.completed"/>
@@ -23,6 +26,9 @@
           </button>
           <button class="border" @click="deleteTask(task)">
             <Delete />
+          </button>
+          <button class="border">
+            <Draggable />
           </button>
         </div>
       </div>
@@ -38,6 +44,7 @@ import Checkbox from '~icons/carbon/checkbox'
 import CheckboxCheckedFilled from '~icons/carbon/checkbox-checked-filled'
 import Edit from '~icons/carbon/edit'
 import Delete from '~icons/carbon/delete'
+import Draggable from '~icons/carbon/draggable'
 import { uuid } from './../../utils'
 
 export default {
@@ -46,11 +53,14 @@ export default {
     CheckboxCheckedFilled,
     Edit,
     Delete,
+    Draggable,
   }, 
   data() {
     return {
       taskInput: '',
       tasks: [],
+      fromIndex: 0,
+      toIndex: 0,
     }
   },
   methods: {
@@ -74,10 +84,23 @@ export default {
       console.log('edit', task.text)
     },
     deleteTask(task) {
-      console.log('delete', task.text)
       const { id } = task
       const index = this.tasks.findIndex(task => task.id === id)
       this.tasks.splice(index, 1)
+    },
+    dragstart(e, taskIndex) {
+      e.dataTransfer.setData("text/plain", taskIndex)
+      this.fromIndex = taskIndex
+    },
+    dragenter(e, taskIndex) {
+      this.toIndex = taskIndex
+    },
+    drop() {
+      const taskToAdd = this.tasks.splice(this.fromIndex, 1)[0]
+      this.tasks.splice(this.toIndex, 0, taskToAdd)
+
+      this.fromIndex = 0
+      this.toIndex = 0
     },
   },
 }
