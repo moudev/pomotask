d3573f89b91e3c1b87c019e2169d4f8a0483b201
moudev <rigobertomauriciomartinez@gmail.com>
2021-12-24 22:39:33 -0600

    refactor(style): fix dark/light mode colors

--- windi.config.js
+++ windi.config.js
@@ -4,9 +4,11 @@ import { defineConfig } from 'windicss/helpers'
 export default defineConfig({
   // https://windicss.org/features/#shortcuts
   shortcuts: {
-    'bg-main': 'bg-warm-gray-50 dark:(bg-[#181818])',
-    'bg-secondary': 'bg-gray-800',
-    'text-color-main': 'text-gray-800 dark:text-warm-gray-50',
-    'text-color-secondary': 'text-gray-800 dark:text-gray-400',
+    // theme control: https://windicss.org/features/dark-mode.html#class-mode
+    'bg-main': 'bg-warm-gray-50 dark:(bg-[#222626])',
+    'bg-secondary': 'bg-[#CDD1CC]',
+    'text-color-main': 'text-[#222626] dark:text-[#CDD1CC]',
+    'text-color-secondary': 'text-[#8C8C94] dark:text-[#8C8C94]',
+    'border-primary': 'border-[#8C8C94] dark:border-[#6E6D6E]',
   },
 })
