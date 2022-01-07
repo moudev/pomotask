d0ef4e92765a1517300eb09bdb30e861d82a23a5
moudev <rigobertomauriciomartinez@gmail.com>
2022-01-07 00:13:39 -0600

    feat: add github link

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,28 @@
   <div class="bg-main min-h-screen text-color-main overflow-hidden px-2 pb-16 flex flex-col gap-4">
     <Timer />
     <TodoList />
+    <a
+      class="border flex justify-center items-center mt-16 rounded-full"
+      href="https://github.com/moudev/pomotask"
+      target="_blank"
+    >
+      <LogoGithub class="mr-1" />
+      Contribuir
+    </a>
   </div>
 </template>
 
 <script>
 import Timer from './components/Timer/Timer.vue'
 import TodoList from './components/TodoList/TodoList.vue'
+import LogoGithub from '~icons/carbon/logo-github'
 
 export default {
   name: 'App',
   components: {
     Timer,
     TodoList,
+    LogoGithub,
   },
 }
 </script>
